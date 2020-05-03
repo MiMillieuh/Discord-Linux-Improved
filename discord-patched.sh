8e8039b2c7d6079c1906f621a8e8a8847238cfba
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2020-05-03 14:01:05 +0200

    Update discord-patched.sh

--- discord-patched.sh
+++ discord-patched.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
-wget discordapp.com/api/download?platform=linux&format=tar.gz
+wget --content-disposition "http://discordapp.com/api/download?platform=linux&format=tar.gz"
 tar -zxf discord-0.0.10.tar.gz --directory /opt
 wget -P /usr/share/applications https://www.dropbox.com/s/8mmk8j7bi3yvnbw/discord.desktop
