e8845cc6632cc18211d51bc52d87017bb51bbbf6
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2020-05-03 13:48:26 +0200

    Add files via upload

---
+++ discord-patched.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget discordapp.com/api/download?platform=linux&format=tar.gz
+tar -zxf discord-0.0.10.tar.gz --directory /opt
+wget -P /usr/share/applications https://www.dropbox.com/s/8mmk8j7bi3yvnbw/discord.desktop
