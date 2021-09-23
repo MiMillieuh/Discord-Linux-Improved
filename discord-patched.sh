80d992085cc87e2bbe29f80c3f119b9018d98084
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2021-09-23 11:35:45 +0200

    Work in progress not working

--- discord-patched.sh
+++ discord-patched.sh
@@ -1,4 +1,5 @@
 #!/bin/su root
 wget --content-disposition "http://discordapp.com/api/download?platform=linux&format=tar.gz"
-tar -zxf discord-0.0.10.tar.gz --directory /opt
+tar -zxf discord-*.tar.gz --directory /opt
+rm -rf discord-*.tar.gz
 wget -P /usr/share/applications https://www.dropbox.com/s/8mmk8j7bi3yvnbw/discord.desktop
