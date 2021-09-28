f061e3ed2e3fed13cf7d375a80583b7937290bac
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2021-09-28 23:15:46 +0200

    Add files via upload

--- updater.sh
+++ updater.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 (
-pkexec bash -c 'rm -rf discord*.tar.gz* ; rm -rf discord*.tar.gz ; wget --content-disposition "http://discordapp.com/api/download?platform=linux&format=tar.gz" -P /opt; echo "33" ; 
+pkexec bash -c 'rm -rf /opt/discord*.tar.gz* ; rm -rf /opt/discord*.tar.gz ; wget --content-disposition "http://discordapp.com/api/download?platform=linux&format=tar.gz" -P /opt; echo "33" ; 
 echo "# Installing Discord..." ;
 rm -rf /opt/Discord ; rm -rf /usr/share/applications/discord.desktop ; tar -zxf /opt/discord-*.tar.gz --directory /opt ; VER=$(ls | grep discord-) ; echo $VER > /opt/Discord/ver.txt ; sleep 5 ; rm -rf /opt/discord-*.tar.gz ; echo "66" ; echo "# Applying patches..." ; wget -P /opt/Discord https://raw.githubusercontent.com/MiMillieuh/Discord-Linux-Improved/master/updater.sh ; wget -P /opt/Discord https://raw.githubusercontent.com/MiMillieuh/Discord-Linux-Improved/master/filechecker.sh ; wget -P /usr/share/applications https://raw.githubusercontent.com/MiMillieuh/Discord-Linux-Improved/master/discord.desktop ;' ; echo "100" ;
 echo "DONE" 
