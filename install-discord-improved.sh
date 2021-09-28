53e01e2b73c783926f7eb8a99970c15a79d0a15e
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2021-09-28 22:52:27 +0200

    Update install-discord-improved.sh

--- install-discord-improved.sh
+++ install-discord-improved.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P /opt/Discord https://raw.githubusercontent.com/MiMillieuh/Discord-Linux-Improved/master/updater.sh
+wget https://raw.githubusercontent.com/MiMillieuh/Discord-Linux-Improved/master/updater.sh
 sh updater.sh
 rm -rf updater.sh
