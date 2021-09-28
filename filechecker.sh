9af0b2e077f864296422b0dcd58ba32d0bfede0d
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2021-09-28 23:46:42 +0200

    Add files via upload

--- filechecker.sh
+++ filechecker.sh
@@ -1,8 +1,8 @@
 INPUT=$(curl -L --head -w '%{url_effective}' "http://discordapp.com/api/download?platform=linux&format=tar.gz"  2>/dev/null | tail -n1)
 SUBSTRING=$(echo $INPUT| cut -d'/' -f 7)
 INPUT2=$(cat /opt/Discord/ver.txt)
-if [[ "$INPUT2" == "$SUBSTRING" ]]
+if [ "$INPUT2" == "$SUBSTRING" ]
 then 
 /opt/Discord/Discord
-else echo  "${INPUT2} ${SUBSTRING}" ; sh /opt/Discord/updater.sh
+else echo  "${INPUT2} "et" ${SUBSTRING}" ; sh /opt/Discord/updater.sh
 fi
