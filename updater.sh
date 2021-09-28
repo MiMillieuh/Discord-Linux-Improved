8f99967846b8e5dfe8f8ef93fa7746f5ebf6af7f
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2021-09-28 23:54:04 +0200

    Add files via upload

--- updater.sh
+++ updater.sh
@@ -10,7 +10,7 @@ zenity --progress --auto-close --auto-kill\
   --text="Downloading Discord..." \
   --percentage=0
 
-sh /opt/Discord/filechecker.sh 
+bash /opt/Discord/filechecker.sh 
 
 if [ "$?" = -1 ] ; then
         zenity --error \
